a69f3a24585fecc46c6033ae77f5e8506a012bc3
Sanket Sharma <sharma.sanket272@gmail.com>
2023-09-16 01:51:30 +0530

    Rearranged files

---
+++ python_sim/aruco_calc.py
@@ -0,0 +1,90 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import math
+import tf
+
+# Global parameters
+marker_pos_rel_camera = [ 0 , 0 , 0]
+pos_camera = [0,0,0]
+marker_size  = 58.5937500001 #- [cm]
+img = np.empty([], dtype=np.uint8) # This will contain image frame from camera
+ini_pt = [0,0,0]
+set_id = 1
+
+
+calib_path  = ""
+camera_matrix   = np.loadtxt(calib_path+'cameraMatrix_webcam.txt', delimiter=',')
+camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_webcam.txt', delimiter=',')
+
+#--- 180 deg rotation matrix around the x axis
+R_flip  = np.zeros((3,3), dtype=np.float32)
+R_flip[0,0] = 1.0 # 1.0
+R_flip[1,1] = -1.0 # -1.0
+R_flip[2,2] =-1.0 # -1.0
+
+#--- Define the aruco dictionary
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+parameters =  aruco.DetectorParameters()
+detector = aruco.ArucoDetector(dictionary, parameters)
+
+
+#-- Font for the text in the image
+font = cv2.FONT_HERSHEY_PLAIN
+
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+def rotationMatrixToEulerAngles(R):
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
+
+
+def aru(frame):
+    global pos_camera,l, ini_pt , marker_pos_rel_camera
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #-- Find all the aruco markers in the image
+    corners, ids, rejected = detector.detectMarkers(gray)#, cameraMatrix=camera_matrix, distCoeff=camera_distortion)
+    #print(ids)
+    pos_camera = [0,0,0]
+
+    if ids is not None and ids[0] == set_id:
+
+        ret = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
+
+        #-- Unpack the output, get only the first
+        rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
+
+        #-- Print the tag position in camera frame
+        marker_pos_rel_camera[0] = tvec[0]/100
+        marker_pos_rel_camera[1] = -tvec[1]/100
+        marker_pos_rel_camera[2] = tvec[2]/100
+
+        #-- Obtain the rotation matrix tag->camera
+        R_ct= np.matrix(cv2.Rodrigues(rvec)[0])
+        R_tc= R_ct.T
+
+
+        #-- Now get Position and attitude of the camera respect to the marker
+        pos_camera = -R_tc*np.matrix(tvec).T
+        return marker_pos_rel_camera
---
+++ python_sim/cuav_landing.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+from pymavlink import mavutil
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import math
+import tf
+import video_prec_gazebo
+import aruco_calc
+import logging
+import threading
+import signal
+import time
+import sys
+
+
+takeoff_alt = 5
+target_pos = [2,-2]
+localPos = [0,0,0,0]
+globalPos = [0,0,0,0,0]
+check = 0
+flag = 0
+
+def signal_handler(signal, frame):
+        sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+logging.basicConfig(level=logging.NOTSET)
+
+the_connection = mavutil.mavlink_connection('tcp:127.0.0.1:5762')
+
+the_connection.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" %
+            (the_connection.target_system, the_connection.target_component))
+
+
+class fcuModes:
+    def _init_(self):
+        pass
+
+    def ackMsg(self):
+        msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+        logging.info(msg)
+
+    def setArm(self):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+        self.ackMsg()
+
+
+    def setDisarm(self):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 0, 0, 0, 0, 0, 0, 0)  # to force : 21196
+        self.ackMsg()
+
+    def setGuidedMode(self):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, 1, 4, 0, 0, 0, 0, 0)
+        self.ackMsg()
+
+    def setTakeoff(self,alt=3):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, alt)
+        self.ackMsg()
+
+    def setRTL(self):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
+        self.ackMsg()
+
+    def setAutoLandMode(self):
+        the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                             mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0, 0)
+        self.ackMsg()
+
+    def setPos(self,x=0,y=0,z=-takeoff_alt):
+        the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+            10,
+            the_connection.target_system, the_connection.target_component,
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED, int(0b0000111111111000),
+            x, y, z,
+            0, 0, 0,
+            0, 0, 0, 0, 0
+        ))
+        logging.info(f"Sent {x},{y},{z} coordinates to FCU")
+
+    def setPosLocal(self,x=0,y=0,z=takeoff_alt):
+        the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+            10,
+            the_connection.target_system, the_connection.target_component,
+            mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, int(0b0000111111111000),
+            x, y, z,
+            0, 0, 0,
+            0, 0, 0, 0, 0
+        ))
+        logging.info(f"Sent {x},{y},{z} coordinates to FCU")
+
+    def recvGPS(self):
+        global globalPos
+        msg = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=False)
+        if msg:
+            logging.info("GPS message recieved")
+            globalPos[0] = msg.lat / 1e7
+            globalPos[1] = msg.lon / 1e7
+            globalPos[2] = msg.alt / 1000.0
+            globalPos[3] = msg.relative_alt / 1000.0
+            globalPos[4] = msg.time_boot_ms/1000.0
+            return 1
+        else:
+            #logging.warning("GPS message not recieved yet")
+            return 0
+
+
+    def recvLocal(self):
+        global localPos
+        msg = the_connection.recv_match(type='LOCAL_POSITION_NED', blocking=False)
+
+        if msg:
+            logging.info("Local message recieved")
+            localPos[0] = msg.x
+            localPos[1] = msg.y
+            localPos[2] = msg.z
+            return 1
+        else:
+            #logging.warning("Local message not recieved yet")
+            return 0
+
+
+class Controller:
+    def __init__(self):
+        self.video = video_prec_gazebo.Cam_stream()       # Get video frame
+        self.frame = np.empty([], dtype=np.uint8)
+        self.mode = fcuModes()
+
+    def exec(self):
+        self.frame = self.video.setup()
+        if not self.video.frame_available():
+            self.waitFun()
+            return 0
+        else:
+            return aruco_calc.aru(self.frame)
+
+    def waitFun(self):
+        logging.warning("Waiting to recieve the camera frame")
+
+def main():
+    global check, flag
+    cnt = Controller()
+    mode = fcuModes()
+
+    ########## send few setpoints before shifting to guided mode ##########
+    mode.setGuidedMode()
+
+    time.sleep(1)
+
+    ########## send message recieving intervals for GPS and Local data ##########
+    the_connection.mav.command_long_send(
+                        the_connection.target_system, the_connection.target_component,
+                        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+                        33,
+                        1e6 / 10,
+                        0, 0, 0, 0,
+                        0,
+                )
+
+    the_connection.mav.command_long_send(
+                    the_connection.target_system, the_connection.target_component,
+                    mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+                    32,
+                    1e6 / 10,
+                    0, 0, 0, 0,
+                    0,
+                )
+    # t1 = threading.Thread(target=mode.setPos,args = (target_pos[0], target_pos[1],takeoff_alt,))
+    # t2 = threading.Thread(target=mode.recvGPS)
+    # t1.start()
+    # t2.start()
+
+    ########## add checks for arming to see if it is actually armed or not ##########
+    while not the_connection.motors_armed():
+        mode.setArm()
+        time.sleep(0.5)
+
+    ########## Takeoff and confirm if takeoff is completed before going to the next step ##########
+    mode.setTakeoff(takeoff_alt)
+
+    while True:
+        mode.recvGPS()
+        altErr = abs((globalPos[2]/100 - 1) - takeoff_alt)
+        print(globalPos[2]/100, takeoff_alt)
+        if altErr <= 0.2 and check == 0:
+            time.sleep(3)
+            check = 1
+        elif altErr <= 0.2 and check == 1:
+            logging.info("Takeoff completed")
+            break
+        else:
+            check = 0
+
+
+
+    ########## Go to rough setpoint of the landing(aruco) marker ##########
+    # t1 = threading.Thread(target=mode.setPos,args = (target_pos[0], target_pos[1],takeoff_alt,))
+    # t1.start()
+    mode.setPos(target_pos[0], target_pos[1],-takeoff_alt)
+    logging.info("Setpoint sent")
+
+    ########################## add checks if drone reached the setpoint #######################
+    # while True:
+    #     mode.recvLocal()
+    #     if abs(target_pos[0] - localPos[0]) <= 0.2 and abs(target_pos[1] - localPos[1]) <= 0.2:
+    #         break
+
+
+
+
+    ########## After reaching and getting stable, start aruco search and rest of the precision landing process ##########
+
+    while True:
+        mode.recvLocal()
+        pos = cnt.exec()
+        if not pos:
+            continue
+        else:
+            if abs(target_pos[0] - localPos[0]) <= 0.2 and abs(target_pos[1] - localPos[1]) <= 0.2 and not flag:      ##### confirm if loop is necessary or a single iteration is accurate enough for getting x and y errors under 0
+                mode.setPosLocal(-pos[1],-pos[0],0)
+                time.sleep(2)
+                print(pos)
+                flag = 1
+            elif abs(pos[0]) <= 0.2 and abs(pos[1]) <= 0.2:
+                mode.setAutoLandMode()
+                
+
+
+
+main()
+
+
+########## Plan of action ##########
+# 1. Arm and Takeoff in Guided mode ----- Done
+# 2. Then go to the setpoint ----- Done
+# 3. Then start aruco detection only if camera frames are recieved ----- Done
+# 4. Then calculate distance error and update that to fcu through local coordinates ----- Done
+# 5. Then after error is minimal enough, shift to land mode ----- Done
+
+
+
+
+############ Important features and additions ############
+# add multithreading for recieving data over mavlink from AP ----- Done
